90018c24c4fd445a43fefd10ff443632171fc332
Przemek Szliep <szlipek@gmail.com>
2019-07-29 15:13:52 +0200

    javascript lesson

--- js/script.js
+++ js/script.js
@@ -39,7 +39,53 @@ function titleClickHandler(event){
     article.classList.add('active')
 }
 
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+
+    for (let article of articles) {
+
+        /* get the article id */
+
+        const articleId = article.getAttribute('id');
+
+        /* find and get the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#'+ articleId +'"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+
+        /* insert link into titleList */
+
+        html = html + linkHTML;
+
+    }
+
+    titleList.insertAdjacentHTML('beforeend', html);
+
+}
+
+generateTitleLinks();
+
+
 const links = document.querySelectorAll('.titles a');
+console.log(links)
 
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
